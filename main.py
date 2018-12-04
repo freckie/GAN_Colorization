00dad651ce20170bf9d2c234e2dc4cf9455b3571
Freckie <freckie@frec.kr>
2018-12-05 02:09:39 +0900

    Fixed: train algorithm

--- main.py
+++ main.py
@@ -3,16 +3,25 @@ import torchvision
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+import torch.utils.data.dataloader
+
 from PIL import Image
 from torch.autograd import Variable
 from torchvision import datasets, transforms
 
+from mydataset import MyDataset
 from network import Generator as Net_G
 from network import Discriminator as Net_D
 
 
 if __name__ == '__main__':
     # Data Loader
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    dataset = MyDataset('./data/processed', transform=transform)
+    data_loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=100, shuffle=True)
 
     # 모델 생성
     model_G = Net_G()
@@ -23,4 +32,9 @@ if __name__ == '__main__':
     optim_G = optim.Adam(model_G.parameters())
     optim_D = optim.Adam(model_G.parameters())
 
-    
+    # 학습
+    epochs = 100
+    for epoch in range(epochs):
+        data_loader = list()
+        for batch_idx, (grey, rgb) in enumerate(data_loader):
+            pass
