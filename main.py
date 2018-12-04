bb38d1180872e6a604d9e00c8ce801e262cf9e40
Freckie <freckie@frec.kr>
2018-12-05 02:45:16 +0900

    Fixed: trainging algorithm

--- main.py
+++ main.py
@@ -14,6 +14,19 @@ from network import Generator as Net_G
 from network import Discriminator as Net_D
 
 
+def GAN_Loss(input_, is_real, criterion):
+    # 진짜 샘플
+    if is_real:
+        tmp_tensor = torch.FloatTensor(input_.size()).fill_(1.0)
+        rgbs = Variable(tmp_tensor, requires_grad=False)
+    # 가짜 샘플
+    else:
+        tmp_tensor = torch.FloatTensor(input_.size()).fill_(0.0)
+        rgbs = Variable(tmp_tensor, requires_grad=False)
+    
+    return criterion(input_, rgbs)
+
+
 if __name__ == '__main__':
     # Data Loader
     transform = transforms.Compose([
@@ -29,12 +42,50 @@ if __name__ == '__main__':
 
     # Criterion, Optimizer
     criterion = nn.BCELoss()
+    criterionL1 = nn.L1Loss()
     optim_G = optim.Adam(model_G.parameters())
     optim_D = optim.Adam(model_G.parameters())
 
     # 학습
     epochs = 100
     for epoch in range(epochs):
-        data_loader = list()
-        for batch_idx, (grey, rgb) in enumerate(data_loader):
-            pass
+        for idx, (tensor_grey, tensor_rgb) in enumerate(data_loader):
+            # ==== D 학습 ==== #
+            model_D.zero_grad()
+
+            # 변수
+            grey = Variable(tensor_grey)
+            real_rgb = Variable(tensor_rgb)
+            # 가짜 RGB 생성
+            fake_rgb = model_G(grey)
+
+            # 예측 후 Loss 계산 (가짜 샘플)
+            pred_fake = model_D(grey, fake_rgb)
+            loss_D_fake = GAN_Loss(pred_fake, False, criterion)
+            # 예측 후 Loss 계산 (진짜 샘플)
+            pred_real = model_D(grey, real_rgb)
+            loss_D_real = GAN_Loss(pred_fake, True, criterion)
+
+            # Loss 합산 후 오류 역전파
+            loss_D = (loss_D_fake + loss_D_real) * 0.5
+            loss_D.backward(retain_graph=True)
+            optim_D.step()
+
+            # ==== G 학습 ==== #
+            model_G.zero_grad()
+
+            pred_fake = model_D(grey, fake_rgb)
+            loss_G_GAN = GAN_Loss(pred_fake, True, criterion)
+            loss_G_L1 = criterionL1(fake_rgb, real_rgb)
+
+            # Loss 합산 후 오류 역전파
+            loss_G = loss_G_GAN + loss_G_L1 * 100
+            loss_G.backward()
+            optim_G.step()
+
+            # ==== 학습 로그 ==== #
+            if idx % 10 == 0:
+                print('Epoch [{}/{}] D_Real_Loss: {%.4f}, D_Fake_Loss: {%.4f}, G_Loss: {%.4f}, G_L1_Loss: {%.4f}'.format(
+                    epoch + 1, epochs, loss_D_real.data[0], loss_D_fake.data[0], loss_G_GAN.data[0], loss_G_L1.data[0]))
+
+        torch.save(model_G.state_dict(), './generator.pth')
