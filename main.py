af577e85ab3a19da170a9d65ed430f6a4039f415
Freckie <freckie@frec.kr>
2018-12-05 12:52:05 +0900

    Fixed: train

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import torch
+import logging
 import torchvision
 import torch.nn as nn
+import torchvision.utils
 import torch.optim as optim
 import torch.nn.functional as F
 import torch.utils.data.dataloader
@@ -12,6 +14,11 @@ from torchvision import datasets, transforms
 from mydataset import MyDataset
 from network import Generator as Net_G
 from network import Discriminator as Net_D
+
+
+def denorm(x):
+    out = (x + 1) / 2
+    return out.clamp(0, 1)
 
 
 def GAN_Loss(input_, is_real, criterion):
@@ -28,13 +35,22 @@ def GAN_Loss(input_, is_real, criterion):
 
 
 if __name__ == '__main__':
+    # logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('[%(asctime)s] %(message)s')
+    streamHandler = logging.StreamHandler()
+    streamHandler.setFormatter(formatter)
+    logger.addHandler(streamHandler)
+
     # Data Loader
     transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
     dataset = MyDataset('./data/processed', transform=transform)
-    data_loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=100, shuffle=True)
+    data_loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=10, shuffle=True)
+    logger.info('데이터 로드 완료.')
 
     # 모델 생성
     model_G = Net_G()
@@ -47,7 +63,7 @@ if __name__ == '__main__':
     optim_D = optim.Adam(model_G.parameters())
 
     # 학습
-    epochs = 100
+    epochs = 10
     for epoch in range(epochs):
         for idx, (tensor_grey, tensor_rgb) in enumerate(data_loader):
             # ==== D 학습 ==== #
@@ -84,8 +100,13 @@ if __name__ == '__main__':
             optim_G.step()
 
             # ==== 학습 로그 ==== #
-            if idx % 10 == 0:
-                print('Epoch [{}/{}] D_Real_Loss: {%.4f}, D_Fake_Loss: {%.4f}, G_Loss: {%.4f}, G_L1_Loss: {%.4f}'.format(
-                    epoch + 1, epochs, loss_D_real.data[0], loss_D_fake.data[0], loss_G_GAN.data[0], loss_G_L1.data[0]))
+            logger.info('Epoch [{}/{}] IDX [{}/{}] D_Real_Loss: {:.6f}, D_Fake_Loss: {:.6f}, G_Loss: {:.6f}, G_L1_Loss: {:.6f}'.format(
+                epoch + 1, epochs, idx + 1, len(data_loader), loss_D_real.data[0], loss_D_fake.data[0], loss_G_GAN.data[0], loss_G_L1.data[0]))
 
-        torch.save(model_G.state_dict(), './generator.pth')
+            # IDX 10번마다 모델 저장 및 샘플 생성
+            if idx % 10 == 0:
+                torch.save(model_G.state_dict(), './generator.pth')
+                # G(grey)
+                temp = model_G(tensor_grey)
+                torchvision.utils.save_image(denorm(temp), './result/{}_{}_fake.jpg'.format(epoch + 1, idx + 1))
+                torchvision.utils.save_image(denorm(tensor_rgb), './result/{}_{}_real.jpg'.format(epoch + 1, idx + 1))
